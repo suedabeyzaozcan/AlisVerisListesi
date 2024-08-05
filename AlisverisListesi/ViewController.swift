d0418578dae3db3966bf40377ce6d1a2a949f9e1
Sueda Beyza Özcan <104599567+suedabeyzaozcan@users.noreply.github.com>
2024-08-05 10:46:50 +0300

    new upload

---
+++ AlisverisListesi/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  AlisverisListesi
+//
+//  Created by Sueda Beyza Özcan on 25.07.2024.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var isimDizisi = [String]()
+    var idDizisi = [UUID]()
+    var secilenIsim = ""
+    var secilenUUID : UUID?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        //sağ üst köşeye sembollü button ekleme kodu
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(eklemeButtonuTiklandi))
+        
+        verilerAl()
+    }
+    override func viewWillAppear(_ _animated: Bool){
+                NotificationCenter.default.addObserver(self, selector: #selector(verilerAl), name: NSNotification.Name(rawValue: "VeriGirildi"), object: nil)
+            }
+            
+    @objc func verilerAl() {
+        isimDizisi.removeAll(keepingCapacity: false)
+        idDizisi.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        //veri çekme isteği oluşturma
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+        fetchRequest.returnsObjectsAsFaults = false // !!!çok büyük verilerle çalışırken  kolaylık olmasını sağlar
+        //Veri çekme
+        do {
+            let sonuclar = try context.fetch(fetchRequest)
+            if sonuclar.count > 0 {
+                for sonuc in sonuclar as! [NSManagedObject] {
+                    // if let optiniol da çıkartır
+                    if let isim = sonuc.value(forKey: "isim") as? String {
+                        isimDizisi.append(isim)
+                    }
+                    if let id = sonuc.value(forKey: "id") as? UUID {
+                        idDizisi.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+        } catch {
+            print("hata var!")
+        }
+    }
+  //veri ekleme
+    @objc func eklemeButtonuTiklandi(){
+        secilenIsim = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isimDizisi.count
+    }
+    //satırda yazıcak yazılar
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let cell = UITableViewCell()
+        //cell.textLabel?.text = "test"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        if indexPath.row < isimDizisi.count{
+            cell.textLabel?.text = isimDizisi[indexPath.row]
+        } else {
+            cell.textLabel?.text = "Veri yok"
+        }
+        return cell
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVC = segue.destination as! DetailsViewController
+            destinationVC.secilenUrunIsmi = secilenIsim
+            destinationVC.secilenUrunUUID = secilenUUID
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        secilenIsim = isimDizisi[indexPath.row]
+        secilenUUID = idDizisi[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+
+}
+    
+
